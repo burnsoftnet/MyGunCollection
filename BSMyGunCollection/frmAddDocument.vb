d4d2b85edcb285d91855444d88314c41781e5f19
burnsoftnet <joe.mireles@burnsoft.net>
2017-07-21 00:27:20 -0400

    Added A selected index indicator to properly assign the right file type icon to the document

--- BSMyGunCollection/frmAddDocument.vb
+++ BSMyGunCollection/frmAddDocument.vb
@@ -5,6 +5,7 @@ Imports System.Data.Odbc
 Imports ADODB
 Imports System.Data.OleDb
 Public Class frmAddDocument
+    Dim SelectedFileType As Integer
 
     Private Sub frmAddDocument_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
 
@@ -58,10 +59,11 @@ Public Class frmAddDocument
 
     Private Sub btnBrowse_Click(sender As System.Object, e As System.EventArgs) Handles btnBrowse.Click
         Try
-            OpenFileDialog1.FilterIndex = 3
+            OpenFileDialog1.FilterIndex = 1
             OpenFileDialog1.Filter = "PDF Files(*.pdf)|*.pdf|Text Files(*.txt)|*.txt|Jpg Files(*.jpg)|*.jpg"
             'If OpenFileDialog1.ShowDialog() <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
             If OpenFileDialog1.ShowDialog() <> Windows.Forms.DialogResult.Cancel Then lblSelectedDoc.Text = OpenFileDialog1.FileName
+            SelectedFileType = OpenFileDialog1.FilterIndex
         Catch ex As Exception
             Dim sSubFunc As String = "btnBrowse.Click"
             Call LogError(Me.Name, sSubFunc, Err.Number, ex.Message.ToString)
