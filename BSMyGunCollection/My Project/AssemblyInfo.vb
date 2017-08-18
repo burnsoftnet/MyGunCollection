afa0332c47cc82a5cf5487be03acd983e033892a
burnsoftnet <joe.mireles@burnsoft.net>
2017-08-17 22:00:17 -0400

    Updated DBBackup and Restore, and signed a number of files, and rebuild project for version 6.0.0.8

--- BSMyGunCollection/My Project/AssemblyInfo.vb
+++ BSMyGunCollection/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("6.0.0.*")> 
-<Assembly: AssemblyFileVersion("6.0.0.7")> 
+<Assembly: AssemblyFileVersion("6.0.0.8")> 
