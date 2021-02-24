d581c63003bb3312905108a856cf0452297dd1f8
Joe Mireles <bs.github@burnsoft.net>
2021-02-24 08:24:24 -0500

    added comments

--- BSMyGunCollection/GlobalVars.vb
+++ BSMyGunCollection/GlobalVars.vb
@@ -1,7 +1,12 @@
 Imports BSMyGunCollection.MGC
 Imports System.Data.Odbc
-Imports BSMyGunCollection.oEncrypt
+''' <summary>
+''' Global Variables and Functions
+''' </summary>
 Module GlobalVars
+    ''' <summary>
+    ''' The Owner ID
+    ''' </summary>
     Public OwnerID As String
     Public OwnerName As String
     Public OwnerLIC As String
@@ -45,7 +50,12 @@ Module GlobalVars
     Public Const TESTEXPIRED = False
     Public Const DEBUG_FILE = "mgc.debug.log"
     Public Const DATABASE_NAME = "MGC.mdb"
-    'format the strings to be database firendly
+    ''' <summary>
+    ''' format the strings to be database friendly
+    ''' </summary>
+    ''' <param name="strContent"></param>
+    ''' <param name="sDefault"></param>
+    ''' <returns></returns>
     Public Function FluffContent(ByVal strContent As String, Optional ByVal sDefault As String = " ") As String
         Dim sAns As String = ""
         Try
@@ -59,7 +69,12 @@ Module GlobalVars
         End Try
         Return sAns
     End Function
-    'Format a string value into a double value
+    ''' <summary>
+    ''' Format a string value into a double value
+    ''' </summary>
+    ''' <param name="strContent"></param>
+    ''' <param name="lDefault"></param>
+    ''' <returns></returns>
     Public Function FluffContent(ByVal strContent As String, ByVal lDefault As Double) As Double
         Dim sAns As Double = 0
         Try
@@ -74,7 +89,12 @@ Module GlobalVars
         End Try
         Return sAns
     End Function
-    'format a astring value into a long value
+    ''' <summary>
+    ''' format a astring value into a long value
+    ''' </summary>
+    ''' <param name="strContent"></param>
+    ''' <param name="lDefault"></param>
+    ''' <returns></returns>
     Public Function FluffContent(ByVal strContent As String, ByVal lDefault As Long) As Long
         Dim lAns As Long = 0
         Try
@@ -89,7 +109,13 @@ Module GlobalVars
         End Try
         Return lAns
     End Function
-    'Check to see if a field is required and alert if the value is not blank and alert if it is required
+    ''' <summary>
+    ''' Check to see if a field is required and alert if the value is not blank and alert if it is required
+    ''' </summary>
+    ''' <param name="strValue"></param>
+    ''' <param name="strField"></param>
+    ''' <param name="StrTitle"></param>
+    ''' <returns></returns>
     Public Function IsRequired(ByVal strValue As String, ByVal strField As String, ByVal StrTitle As String) As Boolean
         Dim bAns As Boolean = False
         Try
@@ -105,6 +131,14 @@ Module GlobalVars
         End Try
         Return bAns
     End Function
+    ''' <summary>
+    ''' Verify that the field is filled in
+    ''' </summary>
+    ''' <param name="lValue"></param>
+    ''' <param name="lDefault"></param>
+    ''' <param name="strField"></param>
+    ''' <param name="StrTitle"></param>
+    ''' <returns></returns>
     Public Function IsRequired(ByVal lValue As Long, ByVal lDefault As Long, ByVal strField As String, ByVal StrTitle As String) As Boolean
         Dim bAns As Boolean = False
         Try
@@ -120,6 +154,14 @@ Module GlobalVars
         End Try
         Return bAns
     End Function
+    ''' <summary>
+    ''' Override to verify that the field is required and has information
+    ''' </summary>
+    ''' <param name="lValue"></param>
+    ''' <param name="lDefault"></param>
+    ''' <param name="strField"></param>
+    ''' <param name="StrTitle"></param>
+    ''' <returns></returns>
     Public Function IsRequired(ByVal lValue As Double, ByVal lDefault As Double, ByVal strField As String, ByVal StrTitle As String) As Boolean
         Dim bAns As Boolean = False
         Try
@@ -135,6 +177,14 @@ Module GlobalVars
         End Try
         Return bAns
     End Function
+    ''' <summary>
+    ''' Check to see if the login is enabled in the database
+    ''' </summary>
+    ''' <param name="PWD"></param>
+    ''' <param name="UID"></param>
+    ''' <param name="FW"></param>
+    ''' <param name="FP"></param>
+    ''' <returns></returns>
     Public Function LoginEnabled(ByRef PWD As String, ByRef UID As String, ByRef FW As String, ByRef FP As String) As Boolean
         Dim bAns As Boolean = False
         Try
@@ -193,6 +243,10 @@ Module GlobalVars
         End Try
         Return bAns
     End Function
+    ''' <summary>
+    ''' Get the Owner ID to look up the information and return the owner id
+    ''' </summary>
+    ''' <returns></returns>
     Public Function GetOwnerID() As Integer
         Dim iAns As Integer = 0
         Try
@@ -226,6 +280,11 @@ Module GlobalVars
         End Try
         Return iAns
     End Function
+    ''' <summary>
+    ''' Convert string to a double
+    ''' </summary>
+    ''' <param name="strValue"></param>
+    ''' <returns></returns>
     Public Function ConvToNum(ByVal strValue As String) As Double
         Dim dAns As Double = 0
         Try
@@ -267,6 +326,11 @@ Module GlobalVars
         End Try
         Return dAns
     End Function
+    ''' <summary>
+    ''' Dump information in the debug log if enabled
+    ''' </summary>
+    ''' <param name="sFrom"></param>
+    ''' <param name="sMsg"></param>
     Public Sub Buggerme(ByVal sFrom As String, ByVal sMsg As String)
         If DEBUG_MODE Then
             Dim ObjFS As New BSFileSystem
@@ -275,6 +339,13 @@ Module GlobalVars
             ObjFS.LogDebugFile(sPath, sMessage)
         End If
     End Sub
+    ''' <summary>
+    ''' Dump information into the error log
+    ''' </summary>
+    ''' <param name="sForm"></param>
+    ''' <param name="sProcedure"></param>
+    ''' <param name="iErrNo"></param>
+    ''' <param name="sErrorDesc"></param>
     Public Sub LogError(ByVal sForm As String, ByVal sProcedure As String, ByVal iErrNo As Long, ByVal sErrorDesc As String)
         Try
             Dim ObjFS As New BSMyGunCollection.MGC.BSFileSystem
@@ -285,6 +356,10 @@ Module GlobalVars
             MsgBox(sMsg)
         End Try
     End Sub
+    ''' <summary>
+    ''' Check to see if the picture in the database is a the default picture
+    ''' </summary>
+    ''' <param name="ID"></param>
     Sub CheckDefaultPic(ByVal ID As Long)
         Try
             Dim Obj As New MGC.GlobalFunctions
@@ -294,6 +369,12 @@ Module GlobalVars
             Call LogError("GlobalVars", sSubFunc, Err.Number, ex.Message.ToString)
         End Try
     End Sub
+    ''' <summary>
+    ''' Search string for text value
+    ''' </summary>
+    ''' <param name="sTxt"></param>
+    ''' <param name="sSearch"></param>
+    ''' <returns></returns>
     Public Function Found(ByVal sTxt As String, ByVal sSearch As String) As Boolean
         Dim POS As Long = 0
         Dim bAns As Boolean = False
