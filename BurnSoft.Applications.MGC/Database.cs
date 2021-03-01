7b7551b44eabef937236b0a01e19d936e9778af6
Joe M <bs.github@burnsoft.net>
2021-03-01 10:17:19 -0500

    #40 Coped from commands from the database library that is on github

--- BurnSoft.Applications.MGC/Database.cs
+++ BurnSoft.Applications.MGC/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Data.Odbc;
@@ -52,7 +53,218 @@ namespace BurnSoft.Applications.MGC
         private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
         #endregion
         //End Snippet
-        public OdbcConnection Conn;
+        private OdbcConnection _conn;
+        #region "Connection Strings"
 
+        /// <summary>
+        /// Connection String Format Used to Connect to MS Access Databases using the Microsoft Access Driver
+        /// </summary>
+        /// <param name="databasePath"></param>
+        /// <param name="databaseName"></param>
+        /// <param name="errOut"></param>
+        /// <param name="password"></param>
+        /// <returns>string</returns>
+        /// <example>
+        /// SEE UNIT TEST @ UnitTest_MSAccess <br/>
+        /// <br/>
+        /// string value = MSAccessDatabase.ConnectionString("C:\\test", "test.mdb", out errOut);
+        /// <br/>
+        /// <b>Results</b><br/>
+        /// Driver={Microsoft Access Driver (*.mdb)};dbq=C:\test\test.mdb
+        /// </example>
+        public static string ConnectionString(string databasePath, string databaseName, out string errOut, string password = "")
+        {
+            string sAns = "";
+            errOut = @"";
+            try
+            {
+                sAns = password?.Length > 0 ? $"Driver={{Microsoft Access Driver (*.mdb)}};dbq={databasePath}\\{databaseName};Pwd={password}" : $"Driver={{Microsoft Access Driver (*.mdb)}};dbq={databasePath}\\{databaseName}";
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("ConnectionString", e);
+            }
+            return sAns;
+        }
+
+        /// <summary>
+        /// Connections to the MS Access Database using the string OLE.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="errOut"></param>
+        /// <param name="password">The password.</param>
+        /// <returns>System.String.</returns>
+        /// <example>
+        /// SEE UNIT TEST @ UnitTest_MSAccess <br/>
+        /// <br/>
+        /// string value = MSAccessDatabase.ConnectionStringOLE("C:\\test", "test.mdb", out errOut);<br/>
+        /// <br/>
+        /// <b>Results</b><br/>
+        /// Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;Data Source="C:\test\test.mdb";
+        /// </example>
+        public static string ConnectionStringOle(string databasePath, string databaseName, out string errOut, string password = "")
+        {
+            string sAns = "";
+            errOut = @"";
+            try
+            {
+                sAns = password?.Length > 0 ? $"Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;Data Source=\"{databasePath}\\{databaseName}\";Jet OLEDB:Database Password={password};" : $"Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;Data Source=\"{databasePath}\\{databaseName}\";";
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("ConnectionString", e);
+            }
+            return sAns;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Connects the database using the connection string.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <example>
+        /// SEE UNIT TEST @ UnitTest_MSAccess <br/>
+        /// <br/>
+        /// MSAccessDatabase obj = new MSAccessDatabase(); <br/>
+        /// bool value = obj.ConnectDB(ConnString, out errOut); <br/>
+        /// obj.Close(out errOut); <br/>
+        /// </example>
+        public bool ConnectDb(string connectionString, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                _conn = new OdbcConnection(connectionString);
+                _conn.Open();
+                bAns = true;
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("ConnectDB", e);
+            }
+            return bAns;
+        }
+        /// <summary>
+        /// Closes the specified error MSG.
+        /// </summary>
+        /// <param name="errMsg">The error MSG.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <example>
+        /// SEE UNIT TEST @ UnitTest_MSAccess <br/>
+        /// <br/>
+        /// MSAccessDatabase obj = new MSAccessDatabase(); <br/>
+        /// obj.Close(out errOut); <br/>
+        /// </example>
+        public bool Close(out string errMsg)
+        {
+            bool bAns = false;
+            errMsg = @"";
+            try
+            {
+                if (_conn.State != ConnectionState.Closed)
+                {
+                    _conn.Close();
+                }
+                _conn = null;
+                bAns = true;
+            }
+            catch (Exception e)
+            {
+                errMsg = ErrorMessage("CloseDB", e);
+            }
+            return bAns;
+        }
+        /// <summary>
+        /// Connect to the database and execute the SQL statement that you passed.
+        /// In this function, we set the connection object null instead of using the Close Function
+        /// because you might be using that object for something else, and this will take out the connection
+        /// from right under neath you, so we just set that to null instead of a hard close.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <exception cref="System.Exception"></exception>
+        /// <example>
+        /// SEE UNIT TEST @ UnitTest_MSAccess <br/>
+        /// <br/>
+        /// string SQL = "INSERT INTO Gun_Cal(Cal) VALUES('TEST');"; <br/>
+        /// MSAccessDatabase obj = new MSAccessDatabase(); <br/>
+        /// bool value = obj.ConnExec(ConnString, SQL, out errOut); <br/>
+        /// </example>
+        public bool ConnExec(string connectionString, string sql, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                if (ConnectDb(connectionString, out errOut))
+                {
+                    OdbcCommand cmd = new OdbcCommand(sql, _conn);
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                    _conn = null;
+                    bAns = true;
+                }
+                else
+                {
+                    throw new Exception(errOut);
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("ConnExec", e);
+            }
+            return bAns;
+        }
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>DataTable.</returns>
+        /// <exception cref="Exception"></exception>
+        /// <example>
+        /// SEE UNIT TEST @ UnitTest_MSAccess <br/>
+        /// <br/>
+        ///  String SQL = "Select * from Gun_Cal"; <br/>
+        /// MSAccessDatabase obj = new MSAccessDatabase(); <br/>
+        /// DataTable table = obj.GetData(ConnString, SQL, out errOut); <br/>
+        /// string TestValue = @""; <br/>
+        ///    foreach(DataRow row in table.Rows) <br/>
+        ///    { <br/>
+        ///        TestValue += String.Format("{0}{1}",row["Cal"].ToString(),Environment.NewLine); <br/>
+        ///    } <br/>
+        /// </example>
+        public DataTable GetData(string connectionString, string sql, out string errOut)
+        {
+            DataTable table = new DataTable();
+            errOut = @"";
+            try
+            {
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                if (ConnectDb(connectionString, out errOut))
+                {
+                    OdbcCommand cmd = new OdbcCommand(sql, _conn);
+                    OdbcDataAdapter rs = new OdbcDataAdapter { SelectCommand = cmd };
+                    rs.Fill(table);
+                }
+                else
+                {
+                    throw new Exception(errOut);
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetData", e);
+            }
+            return table;
+        }
     }
 }
