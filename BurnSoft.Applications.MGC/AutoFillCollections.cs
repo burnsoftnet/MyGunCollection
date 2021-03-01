bde6485eba904940c3fcd2f479916171ed4c2de6
Joe M <bs.github@burnsoft.net>
2021-03-01 17:44:53 -0500

    #40  added gun manufacture auto complete

--- BurnSoft.Applications.MGC/AutoFillCollections.cs
+++ BurnSoft.Applications.MGC/AutoFillCollections.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -48,8 +50,16 @@ namespace BurnSoft.Applications.MGC
         /// <param name="e">The e.</param>
         /// <returns>System.String.</returns>
         private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
-        #endregion
-
+        #endregion        
+        /// <summary>
+        /// Mains the collection.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="strColumn">The string column.</param>
+        /// <param name="strTable">The string table.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>AutoCompleteStringCollection.</returns>
+        /// <exception cref="Exception"></exception>
         private static AutoCompleteStringCollection MainCollection(string databasePath, string strColumn, string strTable, out string errOut)
         {
             AutoCompleteStringCollection acscAns = new AutoCompleteStringCollection();
@@ -57,6 +67,18 @@ namespace BurnSoft.Applications.MGC
             try
             {
                 string sql = $"SELECT {strColumn} from {strTable} order by {strColumn} ASC";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+
+                foreach (DataRow d in dt.Rows)
+                {
+                    if (d[strColumn].ToString() != null)
+                    {
+                        acscAns.Add(d[strColumn].ToString());
+                    }
+                }
+
+                if (acscAns.Count == 0) acscAns.Add("N/A");
             }
             catch (Exception e)
             {
@@ -64,6 +86,16 @@ namespace BurnSoft.Applications.MGC
             }
 
             return acscAns;
+        }
+        /// <summary>
+        /// Guns the manufacturer.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>AutoCompleteStringCollection.</returns>
+        public static AutoCompleteStringCollection Gun_Manufacturer(string databasePath, out string errOut)
+        {
+            return MainCollection(databasePath, "Brand", "Gun_Manufacturer", out errOut);
         }
     }
 }
