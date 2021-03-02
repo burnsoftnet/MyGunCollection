5f2f2b3c6cf6e9397beef494575c45abec546d0f
Joe M <bs.github@burnsoft.net>
2021-03-02 16:43:57 -0500

    #40 added gunsmith and appriaser auto fill classes

---
+++ BurnSoft.Applications.MGC/AutoFill/Appraisers.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace BurnSoft.Applications.MGC.AutoFill
+{
+    /// <summary>
+    /// Class for Appraisers autofill.
+    /// </summary>
+    public class Appraisers
+    {
+        /// <summary>
+        /// Gets the unique names of all the Appraisers listed in the database
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>AutoCompleteStringCollection.</returns>
+        public static AutoCompleteStringCollection Name(string databasePath, out string errOut)
+        {
+            return General.MainCollection(databasePath, "aName", "Appriaser_Contact_Details", out errOut);
+        }
+    }
+}
---
+++ BurnSoft.Applications.MGC/AutoFill/GunSmith.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace BurnSoft.Applications.MGC.AutoFill
+{
+    /// <summary>
+    /// Class GunSmith for gun smith auto fill
+    /// </summary>
+    public class GunSmith
+    {
+        /// <summary>
+        /// Gets the unique name of all the Gunsmiths listed in the database
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>AutoCompleteStringCollection.</returns>
+        public static AutoCompleteStringCollection Name(string databasePath, out string errOut)
+        {
+            return General.MainCollection(databasePath, "gName", "GunSmith_Contact_Details", out errOut);
+        }
+    }
+}
