783534e297a28687c54efa862488bd5c8be57690
Joe M <bs.github@burnsoft.net>
2021-03-10 11:59:12 -0500

    #44 Added unit test

---
+++ BurnSoft.Applications.MGC.UnitTest/Firearms/MyGunCollectionTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using BurnSoft.Applications.MGC;
+using BurnSoft.Applications.MGC.Firearms;
+using BurnSoft.Applications.MGC.Types;
+using BurnSoft.Applications.MGC.UnitTest.Settings;
+
+namespace BurnSoft.Applications.MGC.UnitTest.Firearms
+{
+    [TestClass]
+    public class MyGunCollectionTest
+    {
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        /// <value>The test context.</value>
+        public TestContext TestContext { get; set; }
+        /// <summary>
+        /// The error out
+        /// </summary>
+        private string errOut;
+        /// <summary>
+        /// The gun identifier
+        /// </summary>
+        private int GunID;
+        /// <summary>
+        /// The database path
+        /// </summary>
+        private string DatabasePath;
+
+        [TestInitialize]
+        public void Init()
+        {
+            errOut = @"";
+            DatabasePath = Vs2019.GetSetting("DatabasePath", TestContext);
+            GunID = Vs2019.IGetSetting("MyGunCollectionID", TestContext);
+        }
+        [TestMethod, TestCategory("")]
+        public void GetList()
+        {
+            List<GunCollectionList> value = MyCollection.GetList(DatabasePath, GunID, out errOut);
+            if (value.Count > 0)
+            {
+                foreach (GunCollectionList g in value)
+                {
+                    TestContext.Write($"id : {g.Id}");
+                    TestContext.Write($"Full Name: {g.FullName}");
+                    TestContext.Write($"");
+                    TestContext.Write($"");
+                    TestContext.Write($"--------------------------------------");
+                    TestContext.Write($"");
+                }
+            }
+
+            General.HasTrueValue(value.Count > 0, errOut);
+        }
+    }
+}
---
+++ BurnSoft.Applications.MGC.UnitTest/General.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+namespace BurnSoft.Applications.MGC.UnitTest
+{
+    /// <summary>
+    /// Class General Class with way to test the end results and display the information that was returned in the output
+    /// </summary>
+    public class General
+    {
+        /// <summary>
+        /// Determines whether [has true value] [the specified b ans].
+        /// </summary>
+        /// <param name="bAns">if set to <c>true</c> [b ans].</param>
+        /// <param name="errOut">The error out.</param>
+        public static void HasTrueValue(bool bAns, string errOut = "")
+        {
+            if (errOut?.Length > 0)
+            {
+                Debug.Print("ERROR!");
+                Debug.Print(errOut);
+            }
+            Assert.IsTrue(bAns);
+        }
+        /// <summary>
+        /// Determines whether [has false value] [the specified b ans].
+        /// </summary>
+        /// <param name="bAns">if set to <c>true</c> [b ans].</param>
+        /// <param name="errOut">The error out.</param>
+        public static void HasFalseValue(bool bAns, string errOut = "")
+        {
+            if (errOut?.Length > 0)
+            {
+                Debug.Print("ERROR!");
+                Debug.Print(errOut);
+            }
+            Assert.IsFalse(bAns);
+        }
+        /// <summary>
+        /// Determines whether the specified value has value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="errOut">The error out.</param>
+        public static void HasValue(string value, string errOut = "")
+        {
+            bool isLoaded = (value.Length > 0);
+            if (isLoaded)
+            {
+                Debug.Print("Value Returned: {0}", value);
+            }
+            else
+            {
+                Debug.Print("NO VALUE RETURNED!!");
+            }
+            if (errOut?.Length > 0)
+            {
+                Debug.Print("ERROR!");
+                Debug.Print(errOut);
+            }
+            Assert.IsTrue(isLoaded);
+        }
+    }
+}
