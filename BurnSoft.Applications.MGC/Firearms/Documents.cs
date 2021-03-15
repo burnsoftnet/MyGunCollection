56c69f704d705894ec65f237f213c186054953f7
Joe M <bs.github@burnsoft.net>
2021-03-15 11:00:44 -0400

    #40 added doc list type and added function to add to  list from data table

---
+++ BurnSoft.Applications.MGC/Firearms/Documents.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using BurnSoft.Applications.MGC.Types;
+
+namespace BurnSoft.Applications.MGC.Firearms
+{
+    /// <summary>
+    /// Class Documents to manage the doucments section, upload, edit assisgn, and delete
+    /// </summary>
+    public class Documents
+    {
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string ClassLocation = "BurnSoft.Applications.MGC.Firearms.Documents";
+        /// <summary>
+        /// Errors the message for regular Exceptions
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, Exception e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for access violations
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, AccessViolationException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for invalid cast exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, InvalidCastException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message argument exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for argument null exception.
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        #endregion
+
+        public static long GetLastId(string databasePath, out string errOut)
+        {
+            long lAns = 0;
+            errOut = @"";
+            try
+            {
+                string sql = "select top 1 id from Gun_Collection_Docs order by ID DESC";
+
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("", e);
+            }
+
+            return lAns;
+        }
+
+        /// <summary>
+        /// Mies the list.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;BarrelSystems&gt;.</returns>
+        internal static List<DocumentList> MyList(DataTable dt, out string errOut)
+        {
+            List<DocumentList> lst = new List<DocumentList>();
+            errOut = @"";
+            try
+            {
+                foreach (DataRow d in dt.Rows)
+                {
+                    lst.Add(new DocumentList()
+                    {
+                        Id = Convert.ToInt32(d["id"]),
+                        DocName = d["doc_name"].ToString(),
+                        DocDescription = d["doc_description"].ToString(),
+                        DocFilename = d["doc_filename"].ToString(),
+                        Dta = d["dta"].ToString(),
+                        DataFile = d["doc_file"],
+                        Length = Convert.ToInt32(d["length"]),
+                        DataFileThumb = d["doc_thumb"],
+                        DocExt = d["doc_ext"].ToString(),
+                        Category = d["doc_cat"].ToString(),
+                        SyncLastUpdate = d["sync_lastupdate"].ToString()
+
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("MyList", e);
+            }
+            return lst;
+        }
+    }
+}
---
+++ BurnSoft.Applications.MGC/Types/DocumentList.cs
@@ -0,0 +1,65 @@
+﻿
+namespace BurnSoft.Applications.MGC.Types
+{
+    /// <summary>
+    /// Class DocumentList container
+    /// </summary>
+    public class DocumentList
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the document.
+        /// </summary>
+        /// <value>The name of the document.</value>
+        public string DocName { get; set; }
+        /// <summary>
+        /// Gets or sets the document description.
+        /// </summary>
+        /// <value>The document description.</value>
+        public string DocDescription { get; set; }
+        /// <summary>
+        /// Gets or sets the document filename.
+        /// </summary>
+        /// <value>The document filename.</value>
+        public string DocFilename { get; set; }
+        /// <summary>
+        /// Gets or sets the dta.
+        /// </summary>
+        /// <value>The dta.</value>
+        public string Dta { get; set; }
+        /// <summary>
+        /// Gets or sets the data file.
+        /// </summary>
+        /// <value>The data file.</value>
+        public object DataFile { get; set; }
+        /// <summary>
+        /// Gets or sets the length.
+        /// </summary>
+        /// <value>The length.</value>
+        public long Length { get; set; }
+        /// <summary>
+        /// Gets or sets the data file thumb.
+        /// </summary>
+        /// <value>The data file thumb.</value>
+        public object DataFileThumb { get; set; }
+        /// <summary>
+        /// Gets or sets the document ext.
+        /// </summary>
+        /// <value>The document ext.</value>
+        public string DocExt { get; set; }
+        /// <summary>
+        /// Gets or sets the category.
+        /// </summary>
+        /// <value>The category.</value>
+        public string Category { get; set; }
+        /// <summary>
+        /// Gets or sets the synchronize last update.
+        /// </summary>
+        /// <value>The synchronize last update.</value>
+        public string SyncLastUpdate { get; set; }
+    }
+}
