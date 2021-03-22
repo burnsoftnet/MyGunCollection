68c30120349c2554fcd334abcd9f5cb210290676
Joe M <bs.github@burnsoft.net>
2021-03-22 11:11:23 -0400

    #43 Added Classification class

---
+++ BurnSoft.Applications.MGC/Firearms/Classification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BurnSoft.Applications.MGC.Firearms
+{
+    /// <summary>
+    /// Class Classification, handdles all the management of the Gun_Collection_Classification table.
+    /// </summary>
+    public class Classification
+    {
+    }
+}
