8662f6f7a24ea6306c0479ec603d2f209de5149a
Joe M <bs.github@burnsoft.net>
2021-03-23 14:03:49 -0400

    #43 started unit test for classifcation

---
+++ BurnSoft.Applications.MGC.UnitTest/Firearms/ClassifcationTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BurnSoft.Applications.MGC.UnitTest.Firearms
+{
+    [TestClass]
+    public class ClassifcationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
