4263f69d8bad4310c5f65923e036220f3e62b796
Joe M <bs.github@burnsoft.net>
2021-03-23 15:18:47 -0400

    #40 Added Container for the gunsmith contact table

---
+++ BurnSoft.Applications.MGC/PeopleAndPlaces/GunSmiths.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BurnSoft.Applications.MGC.PeopleAndPlaces
+{
+    /// <summary>
+    /// Class GunSmiths, Main class to handle information about the gun smiths in the database
+    /// </summary>
+    public class GunSmiths
+    {
+
+    }
+}
