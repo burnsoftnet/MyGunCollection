e5a5b2383529e1769c0d57b50296fe0981e7daa1
Joe M <bs.github@burnsoft.net>
2021-03-23 16:37:49 -0400

    #44 Added new unit test for gunsmiths

---
+++ BurnSoft.Applications.MGC.UnitTest/PeopleAndPlaces/GunSmithsTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BurnSoft.Applications.MGC.UnitTest.PeopleAndPlaces
+{
+    [TestClass]
+    public class GunSmithsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
