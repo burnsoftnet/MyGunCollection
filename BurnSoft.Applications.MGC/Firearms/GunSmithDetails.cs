aa3e2fa0942a317c040e29b989311eda631e25a9
Joe M <bs.github@burnsoft.net>
2021-03-24 17:43:28 -0400

    #40 Adding delete function

--- BurnSoft.Applications.MGC/Firearms/GunSmithDetails.cs
+++ BurnSoft.Applications.MGC/Firearms/GunSmithDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 // ReSharper disable UnusedMember.Local
@@ -51,7 +52,61 @@ namespace BurnSoft.Applications.MGC.Firearms
         /// <param name="e">The e.</param>
         /// <returns>System.String.</returns>
         private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
-        #endregion                        
+        #endregion
 
+        public static bool Exists(string databasePath,long gunId,string smithName, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                string sql = $"SELECT * from GunSmith_Details where  like '{smithName}%'";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                bAns = dt.Rows.Count > 0;
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Exists", e);
+            }
+
+            return bAns;
+        }
+
+        public static bool Add(string databasePath, long gunId, string smithName, long gunSmithId, string orderDetails, string notes, string startDate, string returnDate, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                string sql = $"INSERT INTO GunSmith_Details(GID,gsmith,gsid,od,notes,sdate,rdate,sync_lastupdate) VALUES({gunId}," +
+                             $"'{smithName}',{gunSmithId},'{orderDetails}','{notes}','{startDate}','{returnDate}',Now())";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Add", e);
+            }
+
+            return bAns;
+        }
+        public static bool Delete(string databasePath, long id, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                string sql = $"DELETE from GunSmith_Details where id={id}";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Delete", e);
+            }
+
+            return bAns;
+        }
     }
 }
