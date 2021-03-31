6d662047c623807c794ad028c1c9bbcea601a33b
Joe M <bs.github@burnsoft.net>
2021-03-31 15:31:58 -0400

    #43 Added function from library to frmAddManufacture

--- BSMyGunCollection/frmAddManufacturer.vb
+++ BSMyGunCollection/frmAddManufacturer.vb
@@ -1,7 +1,7 @@
 Imports BSMyGunCollection.MGC
 
 ''TODO: Replace code from FrmAddManufacturer #4
-
+''TODO: #43 Clean up unsued code.
 ''' <summary>
 ''' Class frmAddManufacturer.
 ''' Implements the <see cref="System.Windows.Forms.Form" />
@@ -25,18 +25,30 @@ Public Class FrmAddManufacturer
         Try
             Dim strMan As String = FluffContent(txtMan.Text)
             If Not IsRequired(strMan, "Manufacturer's Name", Text) Then Exit Sub
-            Dim objGf As New GlobalFunctions
-            If Not objGf.ObjectExistsinDB(strMan, "Brand", "Gun_Manufacturer") Then
-                Dim obj As New BSDatabase
-                Dim sql As String = "INSERT INTO Gun_Manufacturer(Brand,sync_lastupdate) VALUES('" & strMan & "',Now())"
-                obj.ConnExec(sql)
+            Dim errOut as String = ""
+            If Not BurnSoft.Applications.MGC.Firearms.Manufacturers.Exists(DatabasePath, strMan, errOut) Then
+                If Not BurnSoft.Applications.MGC.Firearms.Manufacturers.Add(DatabasePath, strMan, errOut) Then 
+                    Throw New Exception(errOut)
+                End If
                 MsgBox(strMan & " was added to the database!", MsgBoxStyle.Information, Text)
                 txtMan.Text = ""
-                'Close()
-            Else
+            Else 
                 MsgBox(strMan & " already existed in the database!", MsgBoxStyle.Critical, Text)
                 txtMan.Text = ""
             End If
+
+            'Dim objGf As New GlobalFunctions
+            'If Not objGf.ObjectExistsinDB(strMan, "Brand", "Gun_Manufacturer") Then
+            '    Dim obj As New BSDatabase
+            '    Dim sql As String = "INSERT INTO Gun_Manufacturer(Brand,sync_lastupdate) VALUES('" & strMan & "',Now())"
+            '    obj.ConnExec(sql)
+            '    MsgBox(strMan & " was added to the database!", MsgBoxStyle.Information, Text)
+            '    txtMan.Text = ""
+            '    'Close()
+            'Else
+            '    MsgBox(strMan & " already existed in the database!", MsgBoxStyle.Critical, Text)
+            '    txtMan.Text = ""
+            'End If
         Catch ex As Exception
             Dim sSubFunc As String = "btnAdd.Click"
             Call LogError(Name, sSubFunc, Err.Number, ex.Message.ToString)
@@ -49,8 +61,10 @@ Public Class FrmAddManufacturer
     ''' <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
     Private Sub frmAddManufacturer_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
         Try
-            Dim objAf As New AutoFillCollections
-            txtMan.AutoCompleteCustomSource = objAf.Gun_Manufacturer()
+            'Dim objAf As New AutoFillCollections
+            'txtMan.AutoCompleteCustomSource = objAf.Gun_Manufacturer()
+            Dim errOut as String = ""
+            txtMan.AutoCompleteCustomSource = BurnSoft.Applications.MGC.AutoFill.Gun.Manufacturer(DatabasePath, errOut)
         Catch ex As Exception
             Dim sSubFunc As String = "Load"
             Call LogError(Name, sSubFunc, Err.Number, ex.Message.ToString)
