fe7c61a3d3015e91129e44f03906af39435430ee
Joe M <bs.github@burnsoft.net>
2021-04-27 11:14:42 -0400

    #43 removed usused code

--- BSMyGunCollection/frmEditBarrelSystem.vb
+++ BSMyGunCollection/frmEditBarrelSystem.vb
@@ -30,15 +30,6 @@ Public Class FrmEditBarrelSystem
     ''' </summary>
     Sub AutoFill()
         Try
-            'Dim objAf As New AutoFillCollections
-            'txtCal.AutoCompleteCustomSource = objAf.Gun_Cal
-            'txtFeedSys.AutoCompleteCustomSource = objAf.Gun_Collection_FeedSystem
-            'txtSight.AutoCompleteCustomSource = objAf.Gun_Collection_Sights
-            'txtPetLoads.AutoCompleteCustomSource = objAf.Gun_Cal
-            'txtFinish.AutoCompleteCustomSource = objAf.Gun_Collection_Finish
-            'txtAction.AutoCompleteCustomSource = objAf.Gun_Collection_Action
-            'txtPurFrom.AutoCompleteCustomSource = objAf.Gun_Shop_Details
-            'txtSysType.AutoCompleteCustomSource = objAf.Gun_Collection_BarrelSysTypes
             txtCal.AutoCompleteCustomSource =  BurnSoft.Applications.MGC.AutoFill.Gun.Cal(DatabasePath, _errOut)
             If _errOut.Length > 0 Then Throw New Exception(_errOut)
             txtFeedSys.AutoCompleteCustomSource =  BurnSoft.Applications.MGC.AutoFill.GunCollection.Feedsystem(DatabasePath, _errOut)
@@ -83,29 +74,6 @@ Public Class FrmEditBarrelSystem
                 txtPurPrice.Text = Trim(o.PurchasedPrice)
                 txtPurFrom.Text = Trim(o.PurchasedFrom)
             Next
-
-            'Dim sql As String = "SELECT * from Gun_Collection_Ext where ID=" & Bid
-            'Dim obj As New BSDatabase
-            'Call obj.ConnectDB()
-            'Dim cmd As New OdbcCommand(sql, obj.Conn)
-            'Dim rs As OdbcDataReader
-            'rs = cmd.ExecuteReader
-            'While rs.Read
-            '    txtName.Text = Trim(rs("ModelName"))
-            '    txtSysType.Text = Trim(rs("Type"))
-            '    txtCal.Text = Trim(rs("Caliber"))
-            '    txtBarLen.Text = Trim(rs("BarrelLength"))
-            '    txtOvLen.Text = Trim(rs("Height"))
-            '    txtFinish.Text = Trim(rs("Finish"))
-            '    txtAction.Text = Trim(rs("Action"))
-            '    txtFeedSys.Text = Trim(rs("Feedsystem"))
-            '    txtSight.Text = Trim(rs("Sights"))
-            '    txtPetLoads.Text = Trim(rs("PetLoads"))
-            '    txtPurPrice.Text = Trim(rs("PurchasedPrice"))
-            '    txtPurFrom.Text = Trim(rs("PurchasedFrom"))
-            'End While
-            'rs.Close()
-            'obj.CloseDB()
         Catch ex As Exception
             Dim sSubFunc As String = "LoadData"
             Call LogError(Name, sSubFunc, Err.Number, ex.Message.ToString)
@@ -124,7 +92,6 @@ Public Class FrmEditBarrelSystem
     ''' </summary>
     Sub SaveData()
         Try
-            'Dim obj As New BSDatabase
             Dim sName As String = FluffContent(txtName.Text)
             Dim sysType As String = FluffContent(txtSysType.Text)
             Dim cal As String = FluffContent(txtCal.Text)
@@ -137,7 +104,6 @@ Public Class FrmEditBarrelSystem
             Dim petLoads As String = FluffContent(txtPetLoads.Text)
             Dim purPrice As String = FluffContent(txtPurPrice.Text)
             Dim purFrom As String = FluffContent(txtPurFrom.Text)
-            'Dim sql As String 
 
             If Not IsRequired(sName, "Name", Text) Then Exit Sub
             If Not IsRequired(sysType, "System Type", Text) Then Exit Sub
@@ -146,14 +112,6 @@ Public Class FrmEditBarrelSystem
             If Not IsRequired(purFrom, "Purchased From", Text) Then Exit Sub
 
             If Not BurnSoft.Applications.MGC.Firearms.ExtraBarrelConvoKits.Update(DatabasePath, Bid,Gid,sName, cal, stockFinish, barLen, petLoads, fAction, feedSys, sights, purPrice, purFrom, ovalLen, sysType, _errOut ) Then Throw New Exception(_errOut)
-
-            'sql = "UPDATE Gun_Collection_Ext set ModelName='" & sName & "',Caliber='" & cal &
-            '        "',Finish='" & stockFinish & "',BarrelLength='" & barLen & "',PetLoads='" &
-            '        petLoads & "',Action='" & fAction & "',Feedsystem='" & feedSys & "',Sights='" &
-            '        sights & "',PurchasedPrice='" & purPrice & "',PurchasedFrom='" & purFrom &
-            '        "',Height='" & ovalLen & "',Type='" & sysType & "',sync_lastupdate=Now() where ID=" & Bid
-
-            'obj.ConnExec(sql)
             MdiParent.Refresh()
             Close()
         Catch ex As Exception
