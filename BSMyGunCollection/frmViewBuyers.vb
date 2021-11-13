f655168b615e167318e93592e4961fe4b834a3c7
Joe M <bs.github@burnsoft.net>
2021-11-12 21:47:40 -0500

    #42 updated library to fix buyers count issue

--- BSMyGunCollection/frmViewBuyers.vb
+++ BSMyGunCollection/frmViewBuyers.vb
@@ -1,5 +1,3 @@
-'Imports BSMyGunCollection.MGC
-'Imports System.Data.Odbc
 Imports BurnSoft.Applications.MGC.Firearms
 Imports BurnSoft.Applications.MGC.PeopleAndPlaces
 
@@ -54,51 +52,6 @@ Public Class FrmViewBuyers
             Call LogError(Name, sSubFunc, Err.Number, ex.Message.ToString)
         End Try
     End Sub
-
-    'Private Function HasCollectionAttached(ByVal strId As String) As Integer
-    '    Dim iAns As Integer = 0
-    '    Dim sql As String = "SELECT Count(*) as Total from Gun_Collection where ItemSold=1 and BID=" & strId
-    '    Try
-    '        Dim obj As New BSDatabase
-    '        Call obj.ConnectDB()
-    '        Dim cmd As New OdbcCommand(sql, obj.Conn)
-    '        Dim rs As OdbcDataReader
-    '        rs = cmd.ExecuteReader
-    '        If rs.HasRows Then
-    '            While (rs.Read)
-    '                iAns = rs("Total")
-    '            End While
-    '        End If
-    '        rs.Close()
-    '        Call obj.CloseDB()
-    '    Catch ex As Exception
-    '        Dim sSubFunc As String = "HasCollectionAttached"
-    '        Call LogError(Name, sSubFunc, Err.Number, ex.Message.ToString)
-    '    End Try
-    '    Return iAns
-    'End Function
-    'Private Function GetBuyerName(ByVal strId As String) As String
-    '    Dim sAns As String = ""
-    '    Dim sql As String = "SELECT name from Gun_Collection_SoldTo where ID=" & strId
-    '    Try
-    '        Dim obj As New BSDatabase
-    '        Call obj.ConnectDB()
-    '        Dim cmd As New OdbcCommand(sql, obj.Conn)
-    '        Dim rs As OdbcDataReader
-    '        rs = cmd.ExecuteReader
-    '        If rs.HasRows Then
-    '            While (rs.Read)
-    '                sAns = rs("name")
-    '            End While
-    '        End If
-    '        rs.Close()
-    '        Call obj.CloseDB()
-    '    Catch ex As Exception
-    '        Dim sSubFunc As String = "GetBuyerName"
-    '        Call LogError(Name, sSubFunc, Err.Number, ex.Message.ToString)
-    '    End Try
-    '    Return sAns
-    'End Function
     ''' <summary>
     ''' Handles the Click event of the ToolStripButton1 control.
     ''' </summary>
@@ -107,20 +60,15 @@ Public Class FrmViewBuyers
     Private Sub ToolStripButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ToolStripButton1.Click
         Try
             Dim myValue As Long = ListBox1.SelectedValue
-            'Dim strName As String = GetBuyerName(myValue)
             Dim strName As String = Buyers.GetName(DatabasePath, Convert.ToInt32(myValue), _errOut)
             If _errOut.Length > 0 Then Throw New Exception(_errOut)
-            'Dim obj As New BSDatabase
-            'Dim sql As String = "DELETE from Gun_Collection_SoldTo where ID=" & myValue
             Dim sMsg As String = MsgBox("Are you sure that you want to delete " & strName & " from the database.", MsgBoxStyle.YesNo, "Delete a Buyer")
-            'Dim intColTotal As Integer = HasCollectionAttached(myValue)
             Dim intColTotal As Integer = MyCollection.HasCollectionAttached(DatabasePath, Convert.ToInt32(myValue), _errOut)
             If _errOut.Length > 0 Then Throw New Exception(_errOut)
             If sMsg = vbYes Then
                 If intColTotal <> 0 Then
                     MsgBox("Cannot delete " & strName & "! It still has " & intColTotal & " firearms attached to it!", MsgBoxStyle.Critical, "Cannot Delete Buyer")
                 Else
-                    'obj.ConnExec(sql)
                     If Not Buyers.Delete(DatabasePath, Convert.ToInt32(myValue), _errOut) Then Throw New Exception(_errOut)
                     If _errOut.Length > 0 Then Throw New Exception(_errOut)
                     Call RefreshList()
