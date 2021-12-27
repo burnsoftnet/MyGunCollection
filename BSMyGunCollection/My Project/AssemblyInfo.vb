146386a84c78ad98615c5218dec51786a6899ed3
Joe M <bs.github@burnsoft.net>
2021-12-27 15:03:31 -0500

    #51 code clean up on import page

--- BSMyGunCollection/My Project/AssemblyInfo.vb
+++ BSMyGunCollection/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.1.11")> 
-<Assembly: AssemblyFileVersion("6.5.1.11")> 
+<Assembly: AssemblyVersion("6.5.1.12")> 
+<Assembly: AssemblyFileVersion("6.5.1.12")> 
