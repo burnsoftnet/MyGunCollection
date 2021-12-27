fbba920c0cb61230840f724897dca729d8a9e6a6
Joe M <bs.github@burnsoft.net>
2021-12-27 14:59:41 -0500

    #51 updated import and added import to a new database

--- BSMyGunCollection/My Project/AssemblyInfo.vb
+++ BSMyGunCollection/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.1.10")> 
-<Assembly: AssemblyFileVersion("6.5.1.10")> 
+<Assembly: AssemblyVersion("6.5.1.11")> 
+<Assembly: AssemblyFileVersion("6.5.1.11")> 
