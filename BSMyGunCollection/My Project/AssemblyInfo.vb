197028ff6f4d10d5fdc8fd8714df00720d9f6fad
burnsoftnet <bs.github@burnsoft.net>
2022-01-24 15:50:24 -0500

    #60 inc version

--- BSMyGunCollection/My Project/AssemblyInfo.vb
+++ BSMyGunCollection/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("a313b302-46bd-4a3b-a46b-907bb72bf154")> 
+<Assembly: Guid("a313b302-46bd-4a3b-a46b-907bb72bf154")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.1.14")> 
-<Assembly: AssemblyFileVersion("6.5.1.14")> 
+<Assembly: AssemblyVersion("6.5.1.15")>
+<Assembly: AssemblyFileVersion("6.5.1.15")>
