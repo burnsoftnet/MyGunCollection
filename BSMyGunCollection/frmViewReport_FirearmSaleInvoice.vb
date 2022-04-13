16d123dace95eaa8c9c1cb49235571e9b24d4668
BurnSoft <bs.github@burnsoft.net>
2022-04-13 16:02:09 -0400

    #50 Converted more code to use library

--- BSMyGunCollection/frmViewReport_FirearmSaleInvoice.vb
+++ BSMyGunCollection/frmViewReport_FirearmSaleInvoice.vb
@@ -15,34 +15,44 @@ Public Class FrmViewReportFirearmSaleInvoice
     ''' </summary>
     Public UserId As Long
     ''' <summary>
+    ''' The error out
+    ''' </summary>
+    Private _errOut as String
+    ''' <summary>
     ''' Gets the data.
     ''' </summary>
     Sub GetData()
-        Dim ownerRecId As Long = 0
-        Dim ownerName As String = ""
-        Dim ownerAddress As String = ""
-        Dim ownerCity As String = ""
-        Dim ownerState As String = ""
-        Dim ownerZip As String = ""
-        Dim ownerPhone As String = ""
-        Dim ownerCcd As String = ""
-        Dim obj As New GlobalFunctions
-        obj.GetUserSettingsDb(ownerRecId, ownerName, ownerAddress, ownerCity, ownerState, ownerZip, ownerPhone, ownerCcd)
-        Dim parmList As New List(Of ReportParameter)
-        parmList.Add(New ReportParameter("OWNER_Name", ownerName))
-        parmList.Add(New ReportParameter("OWNER_Address", ownerAddress))
-        parmList.Add(New ReportParameter("OWNER_City", ownerCity))
-        parmList.Add(New ReportParameter("OWNER_State", ownerState))
-        parmList.Add(New ReportParameter("OWNER_ZIP", ownerZip))
-        parmList.Add(New ReportParameter("OWNER_Phone", ownerPhone))
-        parmList.Add(New ReportParameter("OWNER_CCD", ownerCcd))
-        parmList.Add(New ReportParameter("USER_ID", UserId))
-        parmList.Add(New ReportParameter("FIREARM_ID", FirearmId))
-        ReportViewer1.LocalReport.SetParameters(parmList)
-        Gun_Collection_SoldToTableAdapter.Fill(MGCDataSet.Gun_Collection_SoldTo)
-        ForSaleDataTableAdapter.Fill(MGCDataSet.ForSaleData)
+        Try
+            'Dim ownerRecId As Long = 0
+            Dim ownerName As String = ""
+            Dim ownerAddress As String = ""
+            Dim ownerCity As String = ""
+            Dim ownerState As String = ""
+            Dim ownerZip As String = ""
+            Dim ownerPhone As String = ""
+            Dim ownerCcd As String = ""
+            'Dim obj As New GlobalFunctions
+            'obj.GetUserSettingsDb(ownerRecId, ownerName, ownerAddress, ownerCity, ownerState, ownerZip, ownerPhone, ownerCcd)
+            Dim ownerRecId As Long = ownerRecId = BurnSoft.Applications.MGC.PeopleAndPlaces.OwnerInformation.GetOwnerId(DatabasePath, ownerName, ownerCcd, ownerAddress, ownerCity, ownerState, ownerZip, ownerPhone, _errOut)
+            If _errOut.Length > 0 Then Throw New Exception(_errOut)
+            Dim parmList As New List(Of ReportParameter)
+            parmList.Add(New ReportParameter("OWNER_Name", ownerName))
+            parmList.Add(New ReportParameter("OWNER_Address", ownerAddress))
+            parmList.Add(New ReportParameter("OWNER_City", ownerCity))
+            parmList.Add(New ReportParameter("OWNER_State", ownerState))
+            parmList.Add(New ReportParameter("OWNER_ZIP", ownerZip))
+            parmList.Add(New ReportParameter("OWNER_Phone", ownerPhone))
+            parmList.Add(New ReportParameter("OWNER_CCD", ownerCcd))
+            parmList.Add(New ReportParameter("USER_ID", UserId))
+            parmList.Add(New ReportParameter("FIREARM_ID", FirearmId))
+            ReportViewer1.LocalReport.SetParameters(parmList)
+            Gun_Collection_SoldToTableAdapter.Fill(MGCDataSet.Gun_Collection_SoldTo)
+            ForSaleDataTableAdapter.Fill(MGCDataSet.ForSaleData)
 
-        ReportViewer1.RefreshReport()
+            ReportViewer1.RefreshReport()
+        Catch ex As Exception
+            Call LogError(Name, "GetData", Err.Number, ex.Message.ToString)
+        End Try
     End Sub
     ''' <summary>
     ''' Handles the Load event of the frmViewReport_FirearmSaleInvoice control.
