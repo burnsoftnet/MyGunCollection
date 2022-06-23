3701aa109ebaaa7f7d9f67dc69c6b736f7885782
BurnSoft <bs.github@burnsoft.net>
2022-06-23 14:31:31 -0400

    #69 Code Clean up to test picture issues

--- BSMyGunCollection.UnitTest/GlobalSuppressions.cs
+++ BSMyGunCollection.UnitTest/GlobalSuppressions.cs
@@ -7,3 +7,6 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~M:BSMyGunCollection.UnitTest.Settings.Vs2019.GeneralSettings~System.Collections.Generic.List{System.Tuple{System.String,System.String}}")]
 [assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "member", Target = "~M:BSMyGunCollection.UnitTest.UI.MainAppTest.Init")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~F:BSMyGunCollection.UnitTest.UI.Collection.AddTest._firearmToView")]
+[assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "member", Target = "~M:BSMyGunCollection.UnitTest.UI.Collection.AddTest.Init")]
+[assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "member", Target = "~M:BSMyGunCollection.UnitTest.UI.Collection.ViewWindowTest.Init")]
