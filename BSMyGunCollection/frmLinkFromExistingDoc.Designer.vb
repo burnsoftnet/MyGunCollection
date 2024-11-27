85ab79fed24899c1f13888b49bb39c4a4d60a652
BurnSoft <bs.github@burnsoft.net>
2024-11-27 13:38:58 -0500

    #58 Added Accessability Tags forms

--- BSMyGunCollection/frmLinkFromExistingDoc.Designer.vb
+++ BSMyGunCollection/frmLinkFromExistingDoc.Designer.vb
@@ -26,24 +26,24 @@ Partial Class frmLinkFromExistingDoc
     'Do not modify it using the code editor.
     <DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New Container()
-        Dim resources As ComponentResourceManager = New ComponentResourceManager(GetType(FrmLinkFromExistingDoc))
-        Me.MGCDataSet = New MGCDataSet()
-        Me.GunCollectionDocsBindingSource = New BindingSource(Me.components)
-        Me.Gun_Collection_DocsTableAdapter = New Gun_Collection_DocsTableAdapter()
-        Me.cmbDoc = New ComboBox()
-        Me.btnAdd = New Button()
-        Me.Label1 = New Label()
-        Me.Label2 = New Label()
-        Me.btnCancel = New Button()
-        CType(Me.MGCDataSet, ISupportInitialize).BeginInit()
-        CType(Me.GunCollectionDocsBindingSource, ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLinkFromExistingDoc))
+        Me.MGCDataSet = New BSMyGunCollection.MGCDataSet()
+        Me.GunCollectionDocsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Gun_Collection_DocsTableAdapter = New BSMyGunCollection.MGCDataSetTableAdapters.Gun_Collection_DocsTableAdapter()
+        Me.cmbDoc = New System.Windows.Forms.ComboBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        CType(Me.MGCDataSet,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.GunCollectionDocsBindingSource,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
         '
         'MGCDataSet
         '
         Me.MGCDataSet.DataSetName = "MGCDataSet"
-        Me.MGCDataSet.SchemaSerializationMode = SchemaSerializationMode.IncludeSchema
+        Me.MGCDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'GunCollectionDocsBindingSource
         '
@@ -52,76 +52,84 @@ Partial Class frmLinkFromExistingDoc
         '
         'Gun_Collection_DocsTableAdapter
         '
-        Me.Gun_Collection_DocsTableAdapter.ClearBeforeFill = True
+        Me.Gun_Collection_DocsTableAdapter.ClearBeforeFill = true
         '
         'cmbDoc
         '
+        Me.cmbDoc.AccessibleDescription = "Title"
+        Me.cmbDoc.AccessibleName = "cmbDoc"
         Me.cmbDoc.DataSource = Me.GunCollectionDocsBindingSource
         Me.cmbDoc.DisplayMember = "doc_name"
-        Me.cmbDoc.FormattingEnabled = True
-        Me.cmbDoc.Location = New Point(48, 47)
+        Me.cmbDoc.FormattingEnabled = true
+        Me.cmbDoc.Location = New System.Drawing.Point(48, 47)
         Me.cmbDoc.Name = "cmbDoc"
-        Me.cmbDoc.Size = New Size(237, 21)
+        Me.cmbDoc.Size = New System.Drawing.Size(237, 21)
         Me.cmbDoc.TabIndex = 0
         Me.cmbDoc.ValueMember = "ID"
         '
         'btnAdd
         '
-        Me.btnAdd.Location = New Point(210, 75)
+        Me.btnAdd.AccessibleDescription = "Save Changes to Database"
+        Me.btnAdd.AccessibleName = "btnAdd"
+        Me.btnAdd.Location = New System.Drawing.Point(210, 75)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New Size(75, 23)
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
         Me.btnAdd.TabIndex = 1
         Me.btnAdd.Text = "Add"
-        Me.btnAdd.UseVisualStyleBackColor = True
+        Me.btnAdd.UseVisualStyleBackColor = true
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New Point(12, 50)
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(12, 50)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New Size(30, 13)
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Title:"
         '
         'Label2
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New Point(12, 20)
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(12, 20)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New Size(281, 13)
+        Me.Label2.Size = New System.Drawing.Size(281, 13)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Select a document from the collection to link to this firearm"
         '
         'btnCancel
         '
-        Me.btnCancel.Location = New Point(48, 75)
+        Me.btnCancel.AccessibleDescription = "Exit without Saving Changes"
+        Me.btnCancel.AccessibleName = "btnCancel"
+        Me.btnCancel.Location = New System.Drawing.Point(48, 75)
         Me.btnCancel.Name = "btnCancel"
-        Me.btnCancel.Size = New Size(75, 23)
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
         Me.btnCancel.TabIndex = 4
         Me.btnCancel.Text = "Cancel"
-        Me.btnCancel.UseVisualStyleBackColor = True
+        Me.btnCancel.UseVisualStyleBackColor = true
         '
         'frmLinkFromExistingDoc
         '
-        Me.AutoScaleDimensions = New SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = AutoScaleMode.Font
-        Me.ClientSize = New Size(301, 110)
+        Me.AccessibleDescription = "Document Collection"
+        Me.AccessibleName = "frmLinkFromExistingDoc"
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(301, 110)
         Me.Controls.Add(Me.btnCancel)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.cmbDoc)
-        Me.Icon = CType(resources.GetObject("$this.Icon"), Icon)
-        Me.MaximizeBox = False
-        Me.MinimizeBox = False
-        Me.Name = "FrmLinkFromExistingDoc"
+        Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
+        Me.MaximizeBox = false
+        Me.MinimizeBox = false
+        Me.Name = "frmLinkFromExistingDoc"
         Me.Text = "Document Collection"
-        CType(Me.MGCDataSet, ISupportInitialize).EndInit()
-        CType(Me.GunCollectionDocsBindingSource, ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+        CType(Me.MGCDataSet,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.GunCollectionDocsBindingSource,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
 
-    End Sub
+End Sub
     Friend WithEvents MGCDataSet As MGCDataSet
     Friend WithEvents GunCollectionDocsBindingSource As BindingSource
     Friend WithEvents Gun_Collection_DocsTableAdapter As Gun_Collection_DocsTableAdapter
