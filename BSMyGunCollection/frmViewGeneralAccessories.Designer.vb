2ab5da5c32e61cce806886bd561b22a76912b4f2
BurnSoft <bs.github@burnsoft.net>
2025-10-30 13:46:34 -0400

    #5 Added based form and components

--- BSMyGunCollection/frmViewGeneralAccessories.Designer.vb
+++ BSMyGunCollection/frmViewGeneralAccessories.Designer.vb
@@ -22,9 +22,62 @@ Partial Class frmViewGeneralAccessories
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmViewGeneralAccessories))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsBtnAdd = New System.Windows.Forms.ToolStripButton()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsBtnAdd})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1172, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsBtnAdd
+        '
+        Me.tsBtnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsBtnAdd.Image = CType(resources.GetObject("tsBtnAdd.Image"), System.Drawing.Image)
+        Me.tsBtnAdd.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsBtnAdd.Name = "tsBtnAdd"
+        Me.tsBtnAdd.Size = New System.Drawing.Size(23, 22)
+        Me.tsBtnAdd.Text = "Add Accessory"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 25)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(1172, 447)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'frmViewGeneralAccessories
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "frmViewGeneralAccessories"
+        Me.ClientSize = New System.Drawing.Size(1172, 472)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "frmViewGeneralAccessories"
+        Me.Text = "General Accessories"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents tsBtnAdd As ToolStripButton
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
