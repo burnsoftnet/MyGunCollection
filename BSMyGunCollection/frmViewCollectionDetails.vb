9f682921b6e068426aced00f3ef1264a4931b2e2
BurnSoft <bs.github@burnsoft.net>
2025-11-13 14:52:10 -0500

    #123 fixed issue with barrel system id on the load

--- BSMyGunCollection/frmViewCollectionDetails.vb
+++ BSMyGunCollection/frmViewCollectionDetails.vb
@@ -755,7 +755,7 @@ Public Class frmViewCollectionDetails
             If _errOut.Length > 0 Then Throw New Exception(_errOut)
 
             For Each l As GunCollectionFullList In lst
-                BsDefaultbarrelsystemid = l.Bid
+                BsDefaultbarrelsystemid = l.DbId
                 BsHasmultibarrels = l.HasExtraBarrels
                 HasDocuments = l.HasDocuments
                 Text = l.FullName + $"  ( ID: {GunId} )"
