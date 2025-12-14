c452984c0aef40dade9bbaa882735ac28a2998fd
BurnSoft <bs.github@burnsoft.net>
2025-12-14 10:03:36 -0500

    #138 updated build version

--- BSMyGunCollection/My Project/AssemblyInfo.vb
+++ BSMyGunCollection/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.0.7.43")>
-<Assembly: AssemblyFileVersion("7.0.7.43")>
+<Assembly: AssemblyVersion("7.0.7.44")>
+<Assembly: AssemblyFileVersion("7.0.7.44")>
