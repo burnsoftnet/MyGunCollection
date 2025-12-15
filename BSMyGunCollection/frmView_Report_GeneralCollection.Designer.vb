904fecce87ff72ce876ee6cf68547cf112d59f1e
BurnSoft <bs.github@burnsoft.net>
2025-12-15 09:47:38 -0500

    #109 Created New Report to test newer componets

---
+++ BSMyGunCollection/frmView_Report_GeneralCollection.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmView_Report_GeneralCollection
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.SuspendLayout
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "BSMyGunCollection.Report_GeneralAccessories.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(-1, -1)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(799, 448)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'frmView_Report_GeneralCollection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "frmView_Report_GeneralCollection"
+        Me.Text = "frmView_Report_GeneralCollection"
+        Me.ResumeLayout(false)
+
+End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+End Class
---
+++ BSMyGunCollection/frmView_Report_GeneralCollection.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmView_Report_GeneralCollection
+    Private Sub frmView_Report_GeneralCollection_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ReportViewer1.RefreshReport
+    End Sub
+End Class
