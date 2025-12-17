ab22084d719b8081c161033694700f044ec166c3
BurnSoft <bs.github@burnsoft.net>
2025-12-17 11:10:07 -0500

    #109 updated form setting

--- BSMyGunCollection/frmView_Report_GeneralCollection.Designer.vb
+++ BSMyGunCollection/frmView_Report_GeneralCollection.Designer.vb
@@ -24,13 +24,23 @@ Partial Class frmView_Report_GeneralCollection
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
-        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
-        Me.MGCDataSet = New BSMyGunCollection.MGCDataSet()
         Me.General_AccessoriesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MGCDataSet = New BSMyGunCollection.MGCDataSet()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.General_AccessoriesTableAdapter = New BSMyGunCollection.MGCDataSetTableAdapters.General_AccessoriesTableAdapter()
-        CType(Me.MGCDataSet,System.ComponentModel.ISupportInitialize).BeginInit
         CType(Me.General_AccessoriesBindingSource,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.MGCDataSet,System.ComponentModel.ISupportInitialize).BeginInit
         Me.SuspendLayout
+        '
+        'General_AccessoriesBindingSource
+        '
+        Me.General_AccessoriesBindingSource.DataMember = "General_Accessories"
+        Me.General_AccessoriesBindingSource.DataSource = Me.MGCDataSet
+        '
+        'MGCDataSet
+        '
+        Me.MGCDataSet.DataSetName = "MGCDataSet"
+        Me.MGCDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'ReportViewer1
         '
@@ -42,18 +52,8 @@ Partial Class frmView_Report_GeneralCollection
         Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.ServerReport.BearerToken = Nothing
-        Me.ReportViewer1.Size = New System.Drawing.Size(800, 450)
+        Me.ReportViewer1.Size = New System.Drawing.Size(917, 450)
         Me.ReportViewer1.TabIndex = 0
-        '
-        'MGCDataSet
-        '
-        Me.MGCDataSet.DataSetName = "MGCDataSet"
-        Me.MGCDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'General_AccessoriesBindingSource
-        '
-        Me.General_AccessoriesBindingSource.DataMember = "General_Accessories"
-        Me.General_AccessoriesBindingSource.DataSource = Me.MGCDataSet
         '
         'General_AccessoriesTableAdapter
         '
@@ -63,12 +63,12 @@ Partial Class frmView_Report_GeneralCollection
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(917, 450)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "frmView_Report_GeneralCollection"
         Me.Text = "frmView_Report_GeneralCollection"
-        CType(Me.MGCDataSet,System.ComponentModel.ISupportInitialize).EndInit
         CType(Me.General_AccessoriesBindingSource,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.MGCDataSet,System.ComponentModel.ISupportInitialize).EndInit
         Me.ResumeLayout(false)
 
 End Sub
