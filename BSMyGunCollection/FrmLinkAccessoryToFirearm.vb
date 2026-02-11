353224795e85e208bc3ebf13321923389d49a4c0
BurnSoft <bs.github@burnsoft.net>
2026-02-11 11:19:13 -0500

    Updated Package for mgc and added new function to simplify the marking

--- BSMyGunCollection/FrmLinkAccessoryToFirearm.vb
+++ BSMyGunCollection/FrmLinkAccessoryToFirearm.vb
@@ -49,9 +49,11 @@ Public Class FrmLinkAccessoryToFirearm
             Else 
                 If GeneralAccessoriesLinking.AttachToFirearm(DatabasePath, AccessoryId, CLng(strFireArmId), _errOut) Then
                     Dim strMsg As String = "Accessory was copied to " & strFireArmName
-                    GeneralAccessories.SetLinkFromGaStatus(DatabasePath, AccessoryId, true, _errOut)
-                    If _errOut.Length > 0 Then Call LogError(Name, "btnAttach.Click", Err.Number, _errOut)
-                    Dim sAns As String = MsgBox(strMsg & Chr(10) & "Do you want to link it to another firearm?", MsgBoxStyle.YesNo, Text)
+                    GeneralAccessories.MarkAsLinkedIfLinked(DatabasePath, AccessoryId, _errOut)
+                    If _errOut.Length > 0 Then Call LogError(Name, "btnAttach.Click.GeneralAccessories.MarkAsLinkedIfLinked", 
+                                                             Err.Number, _errOut)
+                    Dim sAns As String = MsgBox(strMsg & Chr(10) & "Do you want to link it to another firearm?",
+                                                MsgBoxStyle.YesNo, Text)
                     If sAns = vbNo Then Close()
                 Else 
                     MsgBox("Was unable to link Accessory, see error log for more details", MsgBoxStyle.Critical, Text)
--- BSMyGunCollection/frmViewGeneralAccessories.vb
+++ BSMyGunCollection/frmViewGeneralAccessories.vb
@@ -219,9 +219,7 @@ Public Class frmViewGeneralAccessories
     Private Sub MarkAsAttachedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MarkAsAttachedToolStripMenuItem.Click
         Try
             Dim itemId As Long = CLng(dgvGeneralTable.SelectedRows.Item(0).Cells.Item(0).Value)
-            Dim isAttached as Boolean = GeneralAccessoriesLinking.IsAttached(DatabasePath, itemId, errOut)
-            if errOut.Length > 0 Then Throw New Exception(errOut)
-            if Not GeneralAccessories.SetLinkFromGaStatus(DatabasePath, Convert.ToInt32(itemId), isAttached, errOut) then Throw New Exception(errOut)
+            if Not GeneralAccessories.MarkAsLinkedIfLinked(DatabasePath, itemId, errOut) then Throw New Exception(errOut)
             RefreshData()
         Catch ex As Exception
             Call LogError(Name, "MoveToAFirearmToolStripMenuItem_Click", Err.Number, ex.Message.ToString)
